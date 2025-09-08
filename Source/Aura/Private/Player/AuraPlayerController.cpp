f0d251bdbf409a60f3ead48d1be87b60fcadd379
Boustaaja <115188480+Boustaaja@users.noreply.github.com>
2025-09-09 02:37:50 +0300

    Add IMC to player controller begin play.

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,30 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubSystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	// we have to make sure that the PlayerController replicates. For that it has a boolean
+	bReplicates = true;
+
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	// here we need to add the IMC, for that, we need a variable to store it
+	check(AuraContext); // is AuraContext set or not
+	// Subsystems are singletons, and exist only one at a duration of the program. With this we can add MC
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext; // forward declaration. Also need to add EnhancedInput in to Aura.Build.cs so we can access that module
 /**
  * 
  */
@@ -13,5 +14,16 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+	// We greated the input mapping context, and the place to add that in Player controller is in begin play, Begin play is a protected function
+public:
+	// constructor
+	AAuraPlayerController();
+protected:
+	// We need to override Begin play
+	virtual void BeginPlay() override;
+
+private:
+	// first we need a pointer to UIMC
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
