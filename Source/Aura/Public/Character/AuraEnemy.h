a44b89c3b3ca6fb621dda1ca820eacece4a7a30c
Boustaaja <115188480+Boustaaja@users.noreply.github.com>
2025-09-09 20:34:33 +0300

    Add new enemyInterface and implement it on AuraEnemy.

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface // we want to inherit from this class, remember to include it!
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void HighlightActor() override; // We need to override these inherited functions
+	virtual void UnHighligtActor() override;
 };
---
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EnemyInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UEnemyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AURA_API IEnemyInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	// A function that hihglights the enemy when hoovered over. We can use these functions in all actors that implements this interface
+	virtual void HighlightActor() = 0; // 0; means it is now a pure virtual function. needs to be implemented in enemy class
+	virtual void UnHighligtActor() = 0;
+};
