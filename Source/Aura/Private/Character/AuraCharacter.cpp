e2b94335281d8f737d09c69fc63f5e088c5bede5
Boustaaja <115188480+Boustaaja@users.noreply.github.com>
2025-09-09 04:41:31 +0300

    Try to do a commit again

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,20 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	// These are typical parameters for a top down game
+	// we want the character to turn into direction we are moving it.
+	// for that we want to access some things
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// character will orient towards movement with 400 speed
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	GetCharacterMovement()->bConstrainToPlane = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
